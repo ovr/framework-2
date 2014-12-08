74ecdea4984eabdc099e85381e5dfe704f578f23
anolilab <info@anolilab.de>
2014-12-08 17:45:38 +0100

    [0.9.4-dev] fixes

---
+++ src/Brainwave/Session/Interfaces/SessionInterface.php
@@ -0,0 +1,56 @@
+<?php
+namespace Brainwave\Session\Interfaces;
+
+/**
+ * Narrowspark - a PHP 5 framework
+ *
+ * @author      Daniel Bannert <info@anolilab.de>
+ * @copyright   2014 Daniel Bannert
+ * @link        http://www.narrowspark.de
+ * @license     http://www.narrowspark.com/license
+ * @version     0.9.4-dev
+ * @package     Narrowspark/framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Narrowspark is an open source PHP 5 framework, based on the Slim framework.
+ *
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface as BaseSessionInterface;
+
+/**
+ * SessionInterface
+ *
+ * @package Narrowspark/framework
+ * @author  Daniel Bannert
+ * @since   0.9.4-dev
+ *
+ */
+interface SessionInterface extends BaseSessionInterface
+{
+    /**
+     * Get the session handler instance.
+     *
+     * @return \SessionHandlerInterface
+     */
+    public function getHandler();
+
+    /**
+     * Determine if the session handler needs a request.
+     *
+     * @return bool
+     */
+    public function handlerNeedsRequest();
+
+    /**
+     * Set the request on the handler instance.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return void
+     */
+    public function setRequestOnHandler(Request $request);
+}
